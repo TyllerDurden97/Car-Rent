fbfe1968536bf352a4abed91564a30edc2604875
TyllerDurden97 <108276329+TyllerDurden97@users.noreply.github.com>
2023-09-17 21:18:58 +0000

    Deploying to gh-pages from @ TyllerDurden97/Car-Rent@ee1c67c596366c003eb6bd3361bcdb72317d2a95 🚀

---
+++ static/js/454.b4155190.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkCar_Rent=self.webpackChunkCar_Rent||[]).push([[454],{8949:function(e,n,r){r.r(n),r.d(n,{default:function(){return o}});var a=r(6747),c="Home_wrapper__iZIsl",t=r.p+"static/media/rent1.9099bc1e5f015efbcd3a.jpg",u=r(184);function o(){return(0,u.jsx)(a.Z,{className:c,sx:{backgroundImage:"url(".concat(t,")"),backgroundSize:"cover",backgroundPosition:"center center",backgroundRepeat:"no-repeat"}})}}}]);
+//# sourceMappingURL=454.b4155190.chunk.js.map
