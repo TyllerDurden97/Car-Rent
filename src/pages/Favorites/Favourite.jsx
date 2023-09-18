30c4d9e3e1070051e22f655c349f4740417c7be1
TyllerDurden97 <albatros97dn@gmail.com>
2023-09-18 06:27:54 +0300

    style fav

--- src/pages/Favorites/Favourite.jsx
+++ src/pages/Favorites/Favourite.jsx
@@ -2,6 +2,10 @@ import css from '../Favorites/Favourite.module.css';
 // import Filter from '../../components/Filter/Filter';
 import { React, useEffect, useState } from "react";
 import { nanoid } from 'nanoid';
+import Modal from "components/Modal/Modal.jsx";
+import ModalCard from "../../components/ModalCard/ModalCard";
+import svg from '../../images/symbol-defs.svg';
+
 
 
 
@@ -10,6 +14,8 @@ export default function Favourite() {
    // const end = start + itemsPerPage;
    // const paginatedData = itemsToShow.slice(start, end);
    const [favourItems, setFavourItems] = useState([]);
+   const [showModal, setShowModal] = useState(false);
+   const [cardModal, setCardModal] = useState('');
 
 useEffect(() => {
 //   const favouriteItems = JSON.parse(localStorage.getItem('items'));
@@ -31,7 +37,15 @@ useEffect(() => {
       // console.log('после удаления из избр:', favourItems);
    };
 
-   
+    const modalOpen = (id) => {
+      setShowModal(true);
+      const item = favourItems.find(item => item.id === id);
+      setCardModal(item);
+    };
+
+  const modalClose = () => {
+      setShowModal(false);
+  };
 
    
    return (
@@ -54,27 +68,44 @@ useEffect(() => {
                    <img
                      className={css.imageGalleryItemImg}
                      src={img ? `${img}` : `${photoLink}`} alt={model} />
-                  {/* <span>{address.split(' ').splice(-1)}</span> */}
-                  <span>{make}</span>
-                  <span>{model}</span>
-                  <span>{year}</span>
-                  <span>{rentalPrice}</span>
-                  <span>{shortAddress.splice(0)}</span>
-                  <span>{shortAddress.splice(1)}</span>
-                  <span>{rentalCompany}</span>
-                  <span>{type}</span>
-                  <span>{id}</span>
-                  <span>{accessories[0]}</span>
-                  <button
+                  <h3 className={css.cardTitle}>
+                  <div className={css.cardTitleWrap}>  
+                     <span className={css.cardTitleSpan}>{make}</span>
+                     <span className={css.cardTitleSpanBlue}>{model},</span>
+                     <span className={css.cardTitleSpan}>{year}</span>
+                  </div>
+                     <span className={css.cardTitleSpanPrice}>{rentalPrice}</span>
+                  </h3>
+                     <div className={css.carInfoWrap}>
+                     <span className={css.carInfo}>{shortAddress[0]}  </span><span className={css.delimeter}></span>
+                     <span className={css.carInfo}>{shortAddress[1]}  </span><span className={css.delimeter}></span>
+                     <span className={css.carInfo}>{rentalCompany}</span><span className={css.delimeter}></span>
+                  </div>
+                  <div className={css.carInfoBottomWrap}>
+                     <span className={css.carInfo}>{type}</span><span className={css.delimeter}></span>
+                     <span className={css.carInfo}>{id}</span><span className={css.delimeter}></span>
+                     <span className={css.carInfo}>{accessories[0]}</span><span className={css.delimeter}></span>
+                  </div>
+                     <button type="button" className={css.cardBtnLearnMore} onClick={() => modalOpen(id)}>Learn more</button>
+                        <button
                      type='button'
                      onClick={() => handleBtnFavRemoveClick(id)}
-                  className={css.btnFav}
-                  > button</button>
-                  </li> 
+                     className={css.btnFav}>
+                     <svg width="18" height="18">
+                        <use href={`${svg}#icon-heart`}></use>
+                     </svg>
+                     </button>
+               </li> 
+              
             })
             )}  
          </ul>
-         {/* {itemsOnPage.length >= 8 && <ButtonLoadMore onClick={hadleBtnLoadMore} />} */}
+          {showModal && (
+                <Modal onClose={modalClose}>
+                  <ModalCard data={cardModal}/>
+            </Modal>
+                        )}  
+
      </div>
   );
 }
